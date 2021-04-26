f7cd5ef7e82a04ca4464b61ef5004604dbdcf3c6
ahmed <ahm.gmal@gmail.com>
2021-04-26 01:48:17 -0400

    adding search in the news

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const UrlAPI = "https://content.guardianapis.com/search?api-key=c4205d20-e6b5-47aa-965e-6004b584eb4c";
-const APIKey = "";
+const UrlAPI = "https://content.guardianapis.com/search?api-key=";
+const APIKey = "c4205d20-e6b5-47aa-965e-6004b584eb4c";
 
 function GetNews()
 {
-    fetch(UrlAPI)
+    fetch(UrlAPI+APIKey)
+    .then(function(response) {
+          return response.json();
+    })
+    .then(json => renderNews(json));
+}
+
+function GetNewsForSubject(subject)
+{
+    fetch(UrlAPI + APIKey + "&q=" + subject)
     .then(function(response) {
           return response.json();
     })
@@ -13,3 +22,5 @@ function GetNews()
 function renderNews(imgs) {
     console.log(imgs.response.results);
   }
+
+
