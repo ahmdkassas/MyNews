59605c92cd799a1c0e458275b31160cdbae69bfc
ahmed <ahm.gmal@gmail.com>
2021-04-26 05:09:19 -0400

    adding search function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const UrlAPI = "https://content.guardianapis.com/search?api-key=";
 const APIKey = "c4205d20-e6b5-47aa-965e-6004b584eb4c";
 
+document.getElementById("btnSearch").addEventListener("click", function() {
+   const inputSearch = document.getElementById("txtSearch");
+   GetNewsForSubject(inputSearch.value);
+  });
+
+
 GetNews();
 
 function GetNews()
@@ -14,7 +20,7 @@ function GetNews()
 
 
 function renderNews(news) {
-    //console.log(news[0]);
+    console.log(news[0]);
     const newsFeed = document.getElementById('newsFeed');
     const newslist = document.createElement("ul");
     
@@ -26,7 +32,7 @@ function renderNews(news) {
     const articleUrl = document.createElement("a");
     articleUrl.href = news[i].webUrl;
     articleUrl.innerText = "read more";
-    
+
     newsItm.appendChild(h1Tag);
     newsItm.appendChild(articleUrl);
     newslist.appendChild(newsItm);
@@ -37,9 +43,17 @@ function renderNews(news) {
 
   function GetNewsForSubject(subject)
   {
+    clearNewsFeed();
       fetch(UrlAPI + APIKey + "&q=" + subject)
       .then(function(response) {
             return response.json();
       })
-      .then(json => renderNews(json));
+      .then(json => renderNews(json.response.results));
+  }
+
+  function clearNewsFeed(){
+    const newsFeed = document.getElementById('newsFeed');
+    while (newsFeed.firstChild) {
+        newsFeed.removeChild(newsFeed.firstChild);
+    }
   }
